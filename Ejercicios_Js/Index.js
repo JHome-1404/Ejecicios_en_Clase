a7ab0f8fea86ce3fd405efe7fcf6085bdeaed897
Home <jesushome1404@gmail.com>
2023-06-20 10:30:06 -0500

    v2

--- Ejercicios_Js/Index.js
+++ Ejercicios_Js/Index.js
@@ -1,6 +1,6 @@
 // # Control
-let input_control = document.getElementById("input-control");
 let form_control = document.querySelector(".form-control");
+let input_control = document.getElementById("input-control");
 let form_variables = document.querySelector(".form-variables");
 let form_bucles = document.querySelector(".form-bucles");
 
@@ -15,7 +15,6 @@ form_control.addEventListener("submit", () => {
 
 // # Taller 1
 let input_variables = document.getElementById("input-variables");
-
 form_variables.addEventListener("submit", () => {
   switch (input_variables.value) {
     case "1":
@@ -103,14 +102,8 @@ form_variables.addEventListener("submit", () => {
       break;
     case "8":
       function variables_caso8() {
-        function sumaArray(array) {
-          let suma = 0;
-          for (let i = 0; i < array.length; i++) {
-            suma += array[i];
-          }
-          alert(suma);
-        }
         let array = [];
+        let suma = 0;
         let answer = parseInt(prompt("De cuantos datos quieres que sea tu cadena"));
         if (answer == 0) {
           alert("Escribe un numero mayor a 0");
@@ -120,20 +113,23 @@ form_variables.addEventListener("submit", () => {
           let dato = parseInt(prompt("Escribe un dato numerico"));
           array.push(dato);
         }
-        sumaArray(array);
+        array.forEach((e) => {
+          suma += e;
+        });
+        alert(suma);
       }
       variables_caso8();
       break;
     case "9":
       function variables_caso9() {
-        function comparacionArray(array) {
-          for (let i = 0; i < array.length; i++) {
-            if (array[i] < 0) {
-              return true;
-            } else {
-              return false;
+        function detectarNegativo(array) {
+          let control = false;
+          array.forEach((e) => {
+            if (e <= 0) {
+              control = true;
             }
-          }
+          });
+          return control;
         }
         let array = [];
         let answer = parseInt(prompt("De cuantos datos quieres que sea tu cadena"));
@@ -145,12 +141,117 @@ form_variables.addEventListener("submit", () => {
           let dato = parseInt(prompt("Escribe un dato numerico"));
           array.push(dato);
         }
-        let resultado = comparacionArray(array);
+        let resultado = detectarNegativo(array);
         alert(resultado);
       }
       variables_caso9();
       break;
     case "10":
+      function variables_caso10() {
+        function detectarNegativo(cadena) {
+          let control = true;
+          cadena.forEach((e) => {
+            if (e.length < 5) {
+              control = false;
+            }
+          });
+          return control;
+        }
+        let array = [];
+        let answer = prompt("De cuantos datos quieres que sea tu cadena");
+        if (answer == 0) {
+          alert("Escribe un numero mayor a 0");
+          return;
+        }
+        for (let i = 0; i < answer; i++) {
+          let dato = prompt("Escribe una cadena");
+          array.push(dato);
+        }
+        let resultado = detectarNegativo(array);
+        alert(resultado);
+      }
+      variables_caso10();
+      break;
+  }
+});
+
+// # Taller 2
+let input_bucles = document.getElementById("input-bucles");
+form_bucles.addEventListener("submit", () => {
+  switch (input_bucles.value) {
+    case "1":
+      function bucles_caso1() {
+        let suma = 0;
+        let numero = 0;
+        while (numero >= 0) {
+          numero = parseInt(prompt("Ingresa un Numero"));
+          if (numero >= 0) {
+            suma = suma + numero;
+          }
+        }
+        alert(`La suma es ${suma}`);
+      }
+      bucles_caso1();
+      break;
+    case "2":
+      function bucles_caso2() {
+        let array = [];
+        let array_2 = [];
+        for (let i = 1; i < 11; i++) {
+          array.push(i);
+        }
+        for (let i = 10; i > 0; i--) {
+          array_2.push(i);
+        }
+        alert(`Acendente:${array}/Decendente: ${array_2}`);
+      }
+      bucles_caso2();
+      break;
+    case "3":
+      function bucles_caso3() {
+        function getPar_Impar(array) {
+          const respuesta = array.map((e) => {
+            if (e % 2 == 0) {
+              return (e = `Par: ${e}`);
+            } else {
+              return (e = `Impar ${e}`);
+            }
+          });
+          alert(respuesta);
+        }
+        let array = [];
+        let answer = parseInt(prompt("De cuantos datos quieres que sea tu cadena"));
+        if (answer == 0) {
+          alert("Escribe un numero mayor a 0");
+          return;
+        }
+        for (let i = 0; i < answer; i++) {
+          let dato = parseInt(prompt("Escribe un dato numerico"));
+          array.push(dato);
+        }
+        getPar_Impar(array);
+      }
+      bucles_caso3();
+      break;
+    case "4":
+      function bucles_caso4() {
+        let numero_1 = parseInt(prompt("Ingresa un numero"));
+        let numero_2 = parseInt(prompt("Ingresa otro numero"));
+        let resultado = numero_1 * numero_2;
+        alert(`El resultado de multiplicar ${numero_1} por ${numero_2} es ${resultado}`);
+      }
+      bucles_caso4();
+      break;
+    case "5":
+      function bucles_caso5() {
+        let numero = parseInt(prompt("Ingresa un Numero"));
+        if (numero % 2 == 0) {
+          alert(`El numero ${numero} es Primo`);
+          return;
+        }
+        alert(`El numero ${numero} NO es Primo`);
+      }
+      bucles_caso5();
       break;
   }
 });
